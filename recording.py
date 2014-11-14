892a654364b254d34982abca214e761b487e7b7b
arthur-tsang <arthurltsang@gmail.com>
2014-11-13 17:27:54 -0800

    just moved Nodule declaration to recording.py (sorry, forgot to save recording.py last time)

--- recording.py
+++ recording.py
@@ -7,3 +7,7 @@ Recording = namedtuple('Recording', ['id', 'segments'])
 # Defines a particular segment of a call recording. Segments are usually
 # fixed-length; a recording can have an arbitrary number of segments.
 Segment = namedtuple('Segment', ['path', 'features'])
+
+# Defines a "nodule" that estimates language probabilities based on
+# features from a local set of (say 3) nodes and the nodule before it
+Nodule = namedtuple('Nodule', ['features'])
